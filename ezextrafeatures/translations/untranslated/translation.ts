51872b18245f2f67b0fc66979f52fb2235a67a6e
Adrien Loyant <adrien.loyant@te-laval.fr>
2014-10-23 12:43:27 +0200

    norefs : *translations*

--- ezextrafeatures/translations/untranslated/translation.ts
+++ ezextrafeatures/translations/untranslated/translation.ts
@@ -1,5 +1,12 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.0">
-    
+    <defaultcodec></defaultcodec>
+    <context>
+       <name>kernel/navigationpart</name>
+       <message>
+           <source>ezextrafeatures</source>
+           <translation type="unfinished">eZ Extra Features</translation>
+       </message>
+    </context>
 </TS>
